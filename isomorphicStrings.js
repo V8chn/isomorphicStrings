a5a552110710e9ab6435e92e6cc005b9c684c3e6
V8chn <sfzzz.zzzfs@gmail.com>
2026-03-20 19:14:52 +0300

    solutin task

---
+++ isomorphicStrings.js
@@ -0,0 +1,19 @@
+function  isomorphicStrings(s, t) {
+    if (s.length !== t.length) return false;
+
+    let map = new Map();
+    let pam = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i]) && map.get(s[i]) !== t[i]) return false;
+        if (pam.has(t[i]) && pam.get(t[i]) !== s[i]) return false;
+        
+        map.set(s[i], t[i]);
+        pam.set(t[i], s[i]);
+    }
+
+    return true;
+}
+
+console.log(isomorphicStrings('add', 'egg'));
+console.log(isomorphicStrings('f11', 'b23'));
